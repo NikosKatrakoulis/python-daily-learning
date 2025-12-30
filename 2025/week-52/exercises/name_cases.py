eddaaf09be89c07ac4b2f9f6a654a658303a6235
Nikos Katrakoulis <nikoskatrakoulis@gmail.com>
2025-12-30 22:42:40 +0200

    Add Exercises

--- 2025/week-52/exercises/name_cases.py
+++ 2025/week-52/exercises/name_cases.py
@@ -1,3 +1,7 @@
+# Name Cases: Use a variable to represent a person’s name
+# and then print that person’s name in lowercase, uppercase
+#  and title case.
+
 name = 'niKoS'
 print(name.lower())
 print(name.upper())
--- 2025/week-52/exercises/personal_message.py
+++ 2025/week-52/exercises/personal_message.py
@@ -1,2 +1,6 @@
+# Personal Message: Use a variable to represent a person’s name
+# and print a message to that person. Your message should be simple,
+# such as, “Hello Eric, would you like to learn some Python today?”
+
 name = 'Niko'
 print(f"Hello {name}, would you like to learn some Python today?")
