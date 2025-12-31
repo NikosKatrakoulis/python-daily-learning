9cf1276da48eb841a5c1ca982c2940f41c12e74d
Nikos Katrakoulis <nikoskatrakoulis@gmail.com>
2025-12-31 15:08:09 +0200

    Add PCC' studing

--- 2025/week-52/pcc_book/alien_no_points.py
+++ 2025/week-52/pcc_book/alien_no_points.py
@@ -16,3 +16,10 @@ alien_0 = {'color': 'green', 'speed': 'slow'}
 
 point_value = alien_0.get('points', 'No point value assigned.')
 print(point_value)
+
+"""If you leave out the second argument in the call to
+get() and the key doesn’t exist, Python will return the
+value None. The special value None means “no value
+exists.” This is not an error: it’s a special value meant
+to indicate the absence of a value. You’ll see more
+uses for None in Chapter 8. """
