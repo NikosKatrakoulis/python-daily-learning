f2b1eeee8fbba0d38b81009a31ced53360357604
Nikos Katrakoulis <nikoskatrakoulis@gmail.com>
2026-01-08 15:29:11 +0200

    Add PCC studing

---
+++ 2026/week-02/pcc_book/file_reader.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+path = Path('pi_digits.txt')
+contents = path.read_text()
+print(contents)
+
+
+# from pathlib import Path
+
+# print("CWD:", Path.cwd())
+# print("SCRIPT FOLDER:", Path(__file__).parent)
+
+# path = Path("pi_digits.txt")
+# print("LOOKING FOR:", (Path(__file__).parent / "pi_digits.txt"))
+# print("EXISTS?", (Path(__file__).parent / "pi_digits.txt").exists())
+
+# contents = (Path(__file__).parent /
+#             "pi_digits.txt").read_text(encoding="utf-8")
+# print(contents)
